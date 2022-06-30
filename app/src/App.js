01bb473ded5858ed2733934bd93d5ece2a7c3113
Andreas Nicklaus <andreas_nicklaus@web.de>
2022-06-30 12:07:20 +0200

    First complete test

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Squarespace - Easytourist</h1>
-    </div>
-  );
+import {Component} from 'react'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { initialized: false }
+  }
+
+  componentDidMount() {
+    this.setState({initialized: true})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Squarespace - Easytourist</h1>
+        <p>{this.state.initialized ? "Initialized": "Not initialized"}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
