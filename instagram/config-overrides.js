98a695e4762715439b1f45e2db9a76c9de352548
Andreas Nicklaus <andreas_nicklaus@web.de>
2022-07-14 12:22:57 +0200

    Added config-overrides for instagram module

---
+++ instagram/config-overrides.js
@@ -0,0 +1,12 @@
+// config-overrides.js
+module.exports = {
+  webpack: function (config, env) {
+      config.output = {
+        ...config.output, // copy all settings
+        filename: "static/js/[name].js",
+        chunkFilename: "static/js/[name].chunk.js",
+      };
+
+    return config;
+  }
+};
