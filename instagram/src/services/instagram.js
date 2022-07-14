65ee96c14154c35317aeec2ed162bfb194e99a3d
Andreas Nicklaus <andreas_nicklaus@web.de>
2022-07-14 11:58:22 +0200

    Added Instagram Graph API

--- instagram/src/services/instagram.js
+++ instagram/src/services/instagram.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const INSTAGRAM_URL = 'https://graph.instagram.com/'
+const FACEBOOK_URL = 'https://graph.facebook.com/'
 
 // Instagram @andreasnicklaus
 // const PROFILE_ID = 17841406438473112
@@ -9,6 +10,7 @@ const INSTAGRAM_URL = 'https://graph.instagram.com/'
 // Instagram @rs.reisen
 const PROFILE_ID = 17841414836400318
 const ACCESS_TOKEN = 'IGQVJWVk9vcm9XRzg0ZA2VfS2tnalpSWDU0RjBjLXBXejg3SEJmNlZAMaWpzMExza1JydFdfSTN3emMxVWtUaHhJTkFOemI5Vy1wODdONDk4OFM0dW5IS3RKOWFpSVNxRV9SNmdaaHZA3'
+const GRAPH_ACCESS_TOKEN = 'EAAIefpZCDrIcBABHREiERtecYFPnhcf91cFrqMw4YR2395PwcjNJ94HSlOZB7bhPJeS3AauBPGZCjQYBi4vJeO3gIMdZArcQyV8iZC2TEPfGr2aqffPxyVCL7hXMfRyAPMz58moXNGkVB0GeOR1AFWZBweyffXQz1jC5Uv2Lpn7aKqURwlJZCX6lgs2ZAoJAqYzBDcUvIMYKvwZDZD'
 
 class InstagramService {
     getProfileInfo() {
@@ -25,4 +27,25 @@ class InstagramService {
 
 }
 
-export default new InstagramService();
+class InstagramGraphAPI {
+    getProfileInfo() {
+        return axios.get(FACEBOOK_URL + PROFILE_ID, {params: {fields: 'biography,profile_picture_url,username, website,name,media_count,follows_count,followers_count,ig_id', access_token: GRAPH_ACCESS_TOKEN}})
+            .then((response) => {return response.data})
+            .catch((error) => {console.log(error); return null})
+    }
+
+    getMediaInfo() {
+        return axios.get(FACEBOOK_URL + PROFILE_ID + '/media', {params: {fields: 'caption,children,media_type,product_tags,comments_count,is_comment_enabled,like_count,media_product_type,owner,permalink,shortcode,thumbnail_url,timestamp,username,comments,media_url', access_token: GRAPH_ACCESS_TOKEN}})
+            .then((response) => {return response.data.data})
+            .catch((error) => {console.log(error); return null})
+    }
+
+    getStories() {
+        return axios.get(FACEBOOK_URL + PROFILE_ID + '/stories', {params: {fields: '', access_token: GRAPH_ACCESS_TOKEN}})
+            .then((response) => {return response.data.data})
+            .catch((error) => {console.log(error); return []})
+    }
+}
+
+// export default new InstagramService();
+export default new InstagramGraphAPI();
